833d8f5f409cf91aebad610f885163a95f749c89
kimulu <kimulumichael@gmail.com>
2025-05-28 13:11:16 +0300

    new commit

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,4 +1,52 @@
+import UserCard from "../../components/common/UserCard";
 import Header from "../../components/layout/Header";
-export default function User() {
-  return <Header />;
+import { UserProps } from "../../interfaces";
+
+interface UsersPageProps {
+  users: UserProps[];
 }
+
+const Users: React.FC<UsersPageProps> = ({ users }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-semibold">User Profiles</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition">
+            Add User
+          </button>
+        </div>
+
+        <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {users?.map((user: UserProps) => (
+            <UserCard
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              username={user.username}
+              email={user.email}
+              address={user.address}
+              phone={user.phone}
+              website={user.website}
+              company={user.company}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
+
+export default Users;
