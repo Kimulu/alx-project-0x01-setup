a8c3964c92c097f9b4597238c55cab76ac525047
kimulu <kimulumichael@gmail.com>
2025-05-28 13:32:22 +0300

    new commit

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -19,7 +19,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
         </div>
 
         <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts?.map((user: UserProps) => (
+          {posts.map((user: UserProps) => (
             <UserCard
               key={user.id}
               id={user.id}
